ecd731814b3f3547a8ceb1f03a39d73c2d0688ba
afk-2 <abdurahman.khan@gmail.com>
2024-11-22 18:15:56 +0400

    Add courses images, static file serving middleware and 404 error handling.
    - Added middleware to serve static files from the 'static' directory.
    - Implemented a 404 error handler to display a 'File not found!' message for unmatched requests.
    - Uploaded courses images to the 'static' directory.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express"); //Import express
 const app = express(); // Create an express application
+const path = require("path");
+const fs = require("fs");
 const PORT = 3000;
 
 // Logger Middleware
@@ -7,6 +9,25 @@ app.use((req, res, next) => {
     console.log("Request URL: " + req.url);
     console.log("Request date: " + new Date());
     next();
+});
+
+// Static files
+app.use((req, res, next) => {
+    const filePath = path.join(__dirname, "static", req.url);
+    fs.stat(filePath, (error, fileInfo) => {
+        if (error) {
+            next();
+            return
+        }
+        if (fileInfo.isFile()) res.sendFile(filePath);
+        else next();
+    });
+});
+
+// File not found error message
+app.use((req, res, next) => {
+    res.status(404);
+    res.send("File not found!");
 });
 
 app.listen(PORT, () => {
