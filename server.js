776311f76dd212e3fb8c646f89ac2842988ebde7
afk-2 <abdurahman.khan@gmail.com>
2024-11-22 17:29:51 +0400

    Add logger middleware and set up nodemon - Created a logger middleware to log user requests (URL and timestamp). - Installed nodemon to automatically restart the server during development.

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express"); //Import express
 const app = express(); // Create an express application
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
+// Logger Middleware
+app.use((req, res, next) => {
+    console.log("Request URL: " + req.url);
+    console.log("Request date: " + new Date());
+    next();
 });
 
 app.listen(PORT, () => {
